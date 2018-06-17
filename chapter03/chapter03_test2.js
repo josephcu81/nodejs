fcfe8cf1705204bf7619413b4564c1c2daea8829
josephcu <josephcu81@gmail.com>
2018-06-17 22:29:10 +0900

    챕터3 함수 테스트 파일 추가

---
+++ chapter03/chapter03_test2.js
@@ -0,0 +1,8 @@
+var Person = {};
+
+Person['age'] = 20;
+Person['name'] = 'AOA';
+Person.mobile = '010-1111-2222';
+console.log('나이 : %d', Person.age);
+console.log('이름 : %s', Person.name);
+console.log('전화 : %s', Person.mobile);
---
+++ chapter03/chapter03_test4.js
@@ -0,0 +1,7 @@
+var add = function (a, b) {
+	return a+b;
+};
+
+var result = add (10, 11);
+
+console.log('더하기 (10,11) : %d', result);
